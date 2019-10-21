51a87895fb1d1ecad34e4875239cac07c33668f4
michelrm <michelrevollo@gmail.com>
2019-10-21 09:56:20 +0200

    Add files via upload

---
+++ exploreFile.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+face = pd.read_csv("path_face_file)
+doc = pd.read_csv("path_doc_file)
+
+# if you want to explore the data
+face.head() #print the first 10 lines of the file
+
+doc.head() # same for doc document
+
+faceDoc = face.join(doc, lsuffix='user_id', rsuffix='user_id')
+
+# here you can start looking for duplicate attempts and ect...
