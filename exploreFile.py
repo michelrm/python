e10af82f6978a2046e327537e52654ce3626c037
michelrm <michelrevollo@gmail.com>
2019-10-21 10:57:15 +0300

    Update exploreFile.py

--- exploreFile.py
+++ exploreFile.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-face = pd.read_csv("path_face_file)
-doc = pd.read_csv("path_doc_file)
+face = pd.read_csv("path_face_file")
+doc = pd.read_csv("path_doc_file")
                   
 # if you want to explore the data
 face.head() #print the first 10 lines of the file
